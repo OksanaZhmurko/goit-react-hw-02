9190a33a6d5c6255a9f513310bd88b49e08f54fa
OksanaZhmurko <oksanazhmurko@gmail.com>
2024-01-30 00:30:20 +0200

    create

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+
+import { Description } from '../Description/Description';
+
+export const  App = () => {
+  return (
+    <div>
+      <h1>Sip Happens Café</h1>
+ <p>Please leave your feedback about our service by selecting one of the options below.</p>
+ <Description/>
+    </div>
+  );
+};
+export default App;
